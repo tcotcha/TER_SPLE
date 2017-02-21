a921ec245f22731b3334a25ae931d32b2f5bf901
Unknown <florian.carnac@gmail.com>
2017-02-21 14:11:38 +0100

    Generation du sol avec le bruit de Perlin

---
+++ random_platformer/Assets/scripts/Generate_floor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate_floor : MonoBehaviour {
+
+	public GameObject summer_bottom;
+	public GameObject summer_top;
+	public GameObject winter_bottom;
+	public GameObject winter_top;
+
+	public int season;
+	public int width;
+
+	public int heightMultiplier;
+	public float smoothness;
+
+	private int seed;
+
+	void Start () {
+		seed = Random.Range (-10000, 10000);
+		Generate ();
+	}
+
+	public void Generate() {
+		for (int i = 0; i < width; i++) {
+			float h =Mathf.PerlinNoise(seed,i/smoothness)*heightMultiplier;
+			Debug.Log (h);
+			int i_h = Mathf.RoundToInt (h);
+			GameObject selected;
+			if (h > 1) {
+				for (int j = 0; j < i_h; j++) {
+					selected = chooseTile (i_h, j);
+					Instantiate (selected, new Vector2 (i, j), Quaternion.identity);
+				}
+			}
+		}
+	}
+
+	private GameObject chooseTile(int h,int j){
+		if (season == 0) {
+			if (j < h - 1) {
+				return winter_bottom;
+			} else {
+				return winter_top;
+			}
+		} else {
+			if (j < h - 1) {
+				return summer_bottom;
+			} else {
+				return summer_top;
+			}
+		}
+	}
+}
