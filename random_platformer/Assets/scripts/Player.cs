2ec28f6df74a8e441529c36381e47252ae8a7372
Unknown <hugo.malzieux@wanadoo.fr>
2017-02-23 16:10:38 +0100

    Ajout des animations de déplacement et saut
    Ajout des animations de déplacement et saut

--- random_platformer/Assets/scripts/Player.cs
+++ random_platformer/Assets/scripts/Player.cs
@@ -17,10 +17,19 @@ public class Player : MonoBehaviour {
 	float jumpVelocity;
 	Controller2D controller;
 
+	Animator anim;
+
+	public GameObject test;
+
+	bool faceRight;
+
 	void Start () {
 		controller = GetComponent<Controller2D>();
+		anim = test.GetComponent<Animator> ();
+		print (anim);
 		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
 		jumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+		faceRight = true;
 		//print("Gravity :"+ gravity + " jump velocity : "+jumpVelocity);
 	}
 	
@@ -29,14 +38,37 @@ public class Player : MonoBehaviour {
 		if (controller.collisions.above || controller.collisions.below) {
 			velocity.y = 0;
 		}
-
-		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		flipSprite (horizontal);
+		jumpSprite (horizontal);
+		Vector2 input = new Vector2(horizontal, Input.GetAxisRaw("Vertical"));
 		if (Input.GetKeyDown (KeyCode.UpArrow) && controller.collisions.below) {
 			velocity.y = jumpVelocity;
 		}
 		float targetVelocityX = input.x * moveSpeed; 
 		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, controller.collisions.below?accelerationTimeGrounded:accelerationTimeAirborne);
 		velocity.y += gravity * Time.deltaTime;
+
 		controller.Move (velocity * Time.deltaTime);
+	}
+
+	private void flipSprite(float horizontal){
+		if ((horizontal > 0 && !faceRight) || (horizontal < 0 && faceRight)) {
+			faceRight = !faceRight;
+			Vector3 spriteScale = transform.GetChild (0).localScale;
+			spriteScale.x *= -1;
+			transform.GetChild (0).localScale = spriteScale;
+		}			
+	}
+
+	private void jumpSprite(float horizontal){
+		if (controller.collisions.below) {
+			anim.SetBool ("jump", false);
+			anim.SetFloat ("speed", Mathf.Abs(horizontal));
+		} else {
+			anim.SetBool ("jump", true);
+			anim.SetFloat ("speed", 0);
+
+		}
 	}
 }
